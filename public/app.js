8b680935b690796a5c69ed911e6bae6cb74823aa
Mingwei <yangmw@msn.com>
2017-10-03 00:34:04 -0700

    add a skip button to let users skip to the next problem using custom events

--- public/app.js
+++ public/app.js
@@ -36,6 +36,10 @@ learnjs.buildCorrectFlash = function(problemNum) {
   return correctFlash;
 }
 
+learnjs.triggerEvent = function(name, args) {
+  $('.view-container>*').trigger(name, args);
+}
+
 learnjs.landingView = function() {
   return learnjs.template('landing-view');
 }
@@ -65,6 +69,16 @@ learnjs.problemView = function(data) {
 
   view.find('.check-btn').click(checkAnswerClick);
   view.find('.title').text('Problem #' + problemNumber);
+
+  if(problemNumber < learnjs.problems.length) {
+    var buttonItem = learnjs.template('skip-btn');
+    buttonItem.find('a').attr('href', '#problem-' + (problemNumber + 1));
+    $('.nav-list').append(buttonItem);
+    view.bind('removingView', function() {
+      buttonItem.remove();
+    });
+  }
+
   learnjs.applyObject(problemData, view)
   return view;
 }
@@ -78,6 +92,7 @@ learnjs.showView = function(hash) {
   var hashParts = hash.split('-');
   var viewFn = routes[hashParts[0]];
   if(viewFn) {
+    learnjs.triggerEvent('removingView', []);
     $('.view-container').empty().append(viewFn(hashParts[1]));
   }
 }
